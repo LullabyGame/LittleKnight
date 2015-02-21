0848ffa708009c832aa50cfdd78635119e0a24e8
jintongyao <yaoyao.eric@gmail.com>
2015-02-21 22:02:08 +0800

    添加注释

--- Classes/Settings.h
+++ Classes/Settings.h
@@ -2,7 +2,7 @@
 //  Settings.h
 //  LittleKnight
 //
-//  Created by JinTongyao on 1/25/15.
+//  配置文件
 //
 //
 
@@ -11,15 +11,15 @@
 
 const int TEXTURE_SIDE_LENGTH = 102;
 const float SCALE_RATE = 0.8;
-const int TOTAL_ITEM_TYPE = 7;// total type of tile
-const int MATRIX_WIDTH = 9;// the width number of tile matrix
-const int MATRIX_HEIGHT = 9;// the height number of tile matrix
-const int TILE_GAP = 2;//the gap of tiles
-const int LEFT_PADDING = 6;// left padding
-const int BOTTOM_PADDING = 240;// botton padding
-const int TILE_SIDE_LENGTH = 80;// the side lenght of tile
-const int TILE_CHINK = 10; // buffer zones
+const int TOTAL_ITEM_TYPE = 6;// Item的种类(数量)
+const int MATRIX_WIDTH = 9;// 矩阵横轴的Tile数量
+const int MATRIX_HEIGHT = 9;// 矩阵纵轴的Tile数量
+const int TILE_GAP = 2;// Tile之间的间隙宽度
+const int LEFT_PADDING = 6;// 左边距
+const int BOTTOM_PADDING = 240;// 下边距
+const int TILE_SIDE_LENGTH = 80;// Tile边长
+const int TILE_CHINK = 10; // Tile的缓冲区大小
 
-extern int levelProgress;
+extern int levelProgress;// 当前用户的关卡进度
 
 #endif
